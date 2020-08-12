dbf570f93a0730d8c129a2546f51682adb099ff2
Joe <palnorbert@citromail.hu>
2020-08-12 08:50:42 +0200

    New modifications.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,51 @@
 function calcAmount() {
     let price = 1000;
-    let quantityInput = document.querySelector("input[name='quantity-input']");
-    let amount = parseInt(quantityInput.value) * price;
-    let showAmount = document.querySelector("span.show-amount")
+    let amountInput = document.querySelector("input[name='amount-input']");
+    let showAmount = document.querySelector("span.show-amount");
+    let amountNumber = parseInt(amountInput.value);
 
-    showAmount.innerHTML = amount;
+    if (amountNumber > 10) {
+        alert("Maximum 10 terméket vásárolhat egyszerre!");
+    } else if (amountNumber < 1) {
+        alert("Minimum 1 terméket kell tartalmaznia a rendelésnek!")
+    } else {
+        let amount = parseInt(amountInput.value) * price;
+        if (amount < 5000) {
+            amount = amount + 500;
+        } else {
+            alert("5000Ft feletti vásárlás esetén a házhozszállítás ingyenes!");
+        } 
+        showAmount.innerHTML = amount.toFixed();
+    }
+
 }
+
+function inputValidation() {
+    let userAddress = document.querySelector("input[name='address']");
+    let trimAddress = userAddress.value.trim();
+    let presentAddress = document.querySelector("span.show-address");
+
+    if (trimAddress.length < 10) {
+        alert("Kérjük a teljes lakcímed írd be a Lakcím rovatba!");
+    } else {
+        console.log(trimAddress);
+        presentAddress.innerHTML = trimAddress;
+    }
+
+    let emailAddress = document.querySelector("input[name='email']");
+    let showEmail = emailAddress.value;
+    let trimEmail = showEmail.trim();
+
+    if (trimEmail.indexOf('@') < 0 && trimEmail.indexOf('.') < 0) {
+        alert("Az email cím formátuma érvénytelen, kérjük javítsd ki!");
+    } else {
+        console.log(trimEmail);
+    }
+
+}
+
+
+document.getElementById("buttonOrder").addEventListener("click", inputValidation);
 
 function calcWeather() {
     let days = ('Hétfő', 'Kedd', 'Szerda', 'Csütörtök', 'Péntek', 'Szombat', 'Vasárnap')
