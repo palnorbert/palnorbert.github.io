642beb0ee0492403e20d32aa15734e5bdfef7fe5
Joe <palnorbert@citromail.hu>
2020-08-24 17:01:07 +0200

    New scripts.

--- js/weather.js
+++ js/weather.js
@@ -1,20 +1,24 @@
-let homersekletek = [12, 15, 2, 16, 24, 30, 17];
+function weatherOfDay() {
+    let week = [2, 18, -1, 22, 30, 40, -10];
+    let offers = ["Forró Csoki", "Meleg Tea", "Finom Süti", "Fagyi", "Jéghideg Limonádé"];
+    let maxDegree = [0, 15, 20, 25, 100];
+    let weekSelect = document.getElementById("days-of-week");
+    let valueSelectedDay = parseInt(weekSelect.options[weekSelect.selectedIndex].value);
+    let showDegree = document.querySelector("span#today-degree");
+    showDegree.innerHTML = week[valueSelectedDay];
+    let showDegreeFa = document.querySelector("span#today-degreefa");
+    showDegreeFa.innerHTML = week[valueSelectedDay]*9 / 5 + 32;
+    let showOffer = document.querySelector("span#today-offer");
 
-let ajanlat = ["forró csoki",
-               "finom süti",
-               "meleg tea",
-               "fagyi",
-               "jéghideg limonádé",
-               "whisky" ,          
-               "tavaszi saláta" ];
+    for (let i = 0; i < maxDegree.length; i++) {
 
-function idoApp() {
-    let sel = document.querySelector('.form-control').value;
-    let nap = document.querySelector('.show-temp');
-    let fa = document.querySelector('.fahr');
-    let napAjanl = document.querySelector('.ajanlat');
-    napAjanl.innerHTML = ajanlat[sel];
-    nap.innerHTML = homersekletek[sel]; 
-    fa.innerHTML = homersekletek[sel]* 9 / 5 + 32; 
- 
+        if (week[valueSelectedDay] < maxDegree[i]) {
+            showOffer.innerHTML = offers[i];
+            break;
+          
+        }
+    
+    }
+  
+  
 }
