32aa0f49dbf4559ad5d37b5d989663fa53fcfafc
Joe <palnorbert@citromail.hu>
2020-08-29 12:50:33 +0200

    Created temp calculations.

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,5 @@
+let week = [2, 18, -1, 22, 30, 40, -10];
 function weatherOfDay() {
-    let week = [2, 18, -1, 22, 30, 40, -10];
     let offers = ["Forró Csoki", "Meleg Tea", "Finom Süti", "Fagyi", "Jéghideg Limonádé"];
     let maxDegree = [0, 15, 20, 25, 100];
     let weekSelect = document.getElementById("days-of-week");
@@ -20,5 +20,39 @@ function weatherOfDay() {
     
     }
   
-  
 }
+
+let max = week[0];
+for (let i=0; i < week.length; i++) {
+if(week[i]>max){
+    max = week[i];
+}
+}
+document.getElementById("max-temp").innerHTML = "A héten várható legmagasabb hőmérséklet: " + max + "°C"
+
+let min = week[0];
+for (let j =0; j < week.length; j++) {
+    if (week[j] < min) {
+        min = week[j];
+    }
+}
+document.getElementById("min-temp").innerHTML = "A héten várható legalacsonyabb hőmérséklet: " + min + "°C"
+
+let sum = 0;
+for (let k = 0; k< week.length; k++) {
+sum += week[k]
+avg = sum/week.length
+}
+document.getElementById("avg-temp").innerHTML = "A héten várható átlaghőmérséklet: " + avg.toPrecision(4) + "°C"
+
+/*
+Szélsőérték keresés algoritmusa
+
+legnagyobb = első elem
+CIKLUS AMÍG van még szám, ADDIG
+    szám = következő szám
+    HA szám > legnagyobb, AKKOR
+        legnagyobb = szám
+    FELTÉTEL VÉGE
+CIKLUS VÉGE
+*/
